075697aca5610da0fb7365fb1ee7a17e1c19c56a
racuna <rodrigoacuna.dev@gmail.com>
2024-03-03 19:09:52 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="flex justify-center items-center bg-slate-500 h-screen w-screen">
+
+    <h1 class="text-6xl text-white">Repo para pruebas con Git</h1>
+
+  </main>
+</template>
