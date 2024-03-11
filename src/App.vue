df5bca283517827df32633c2a60d6b373879a331
Jano <martinojano0@gmail.com>
2024-03-11 15:34:27 -0300

    upd change h3

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,6 @@
   <main class="flex flex-col justify-center items-center bg-slate-500 h-screen w-screen">
 
     <h1 class="text-6xl text-white">Repo para pruebas con Git</h1>
-    <h3>titulo h3</h3>
+    <h5>ahora es un h5</h5>
   </main>
 </template>
