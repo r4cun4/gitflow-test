e608e74e6854117834153d2e518424903cab1dea
Jano <martinojano0@gmail.com>
2024-03-11 14:57:52 -0300

    upd tag p

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,6 @@
 
     <h1 class="text-6xl text-white">Repo para pruebas con Git</h1>
 
-    <p class="text-white text-6xl m-10">Hola Mundo!</p>
+    <p class="text-white text-6xl m-10">Hola!</p>
   </main>
 </template>
